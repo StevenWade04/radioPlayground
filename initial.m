aac5492c9b4a8a7ed907daf20f645c64c20f4e95
Steven <stevenwade2004@gmail.com>
2025-06-13 20:47:26 +0930

    Modified CustomProtocol and conversion to .txt format

--- initial.m
+++ initial.m
@@ -1,1 +1,9 @@
-disp('Hello, this should hopefully have the radio thing in it in the future, thansk for checking this far back!');!
+clc
+clear
+close all
+
+fprintf('Hello, this should hopefully have the radio thing in it in the future, thansk for checking this far back!\n');
+fprintf(['FYI: The best way to go about programming this will probably be to have everything handled in functions \n' ...
+    'Reading from arduino, handshake, checking odd-parity, decoding\n']);
+fprintf('It would be call inmplementing encryption, transfer of multiple file types\n');
+fprintf("Multiple file types by handling all file data\n");
